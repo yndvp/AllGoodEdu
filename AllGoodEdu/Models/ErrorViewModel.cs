865475df31aa1678c3e01e82859dfe7ef56cc890
yndvp <58580623+yndvp@users.noreply.github.com>
2021-10-10 16:05:33 -0400

    Add project files.

---
+++ AllGoodEdu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AllGoodEdu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
